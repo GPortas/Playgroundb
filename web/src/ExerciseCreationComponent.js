f1e705d483ec2bb9844826026342dad28a61045b
gportas <guillermoportasgonzalez@gmail.com>
2018-02-02 18:25:04 +0100

    Reactjs exercise cration class created

---
+++ web/src/ExerciseCreationComponent.js
@@ -0,0 +1,49 @@
+import './App.css';
+
+var React = require('react');
+var createReactClass = require('create-react-class');
+
+const ExerciseCreationFormComponent = createReactClass({
+    render() {
+        return (
+            <div className="exercise-creation-forms-div">
+                <form>
+                    <div className="form-group">
+                        <label for="inputStatement" className="exercise-creation-label">Write the statement of the
+                            exercise:</label>
+                        <textarea type="statement" className="form-control" id="inputStatement"
+                                  aria-describedby="statement" rows="3"/>
+                        <small id="statementHelp" className="form-text text-muted">Try to be as concise as possible
+                            because this will be the statement that your students will receive.
+                        </small>
+                    </div>
+                    <div className="form-group">
+                        <label for="inputSolution" className="exercise-creation-label">Write the expected result
+                            after executing the necessary query:</label>
+                        <textarea type="solution" className="form-control" id="inputSolution" rows="3"
+                                  placeholder={"{\"data\": {}}"}/>
+                        <small id="solutionHelp" className="form-text text-muted">Remember to verify that the format
+                            is correct.
+                        </small>
+                    </div>
+                    <div className="form-group">
+                        <label for="inputQuery" className="exercise-creation-label">If you wish, you can also obtain
+                            the solution to this exercise by consulting the database:</label>
+                        <textarea type="query" className="form-control queryTextArea" id="inputQuery" rows="6"/>
+                        <button type="submit" className="btn btn-warning exercise-creation-execute-query-button"
+                                id="executeQueryButton">Execute Query
+                        </button>
+                    </div>
+                    <div align="center" className="form-group">
+                        <button type="submit" className="btn btn-danger exercise-creation-execute-cancel-button"
+                                id="cancelButton">Cancell Exercise
+                        </button>
+                        <button type="submit" className="btn btn-success" id="submitButton">Submit Exercise</button>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+});
+
+export default ExerciseCreationFormComponent;
