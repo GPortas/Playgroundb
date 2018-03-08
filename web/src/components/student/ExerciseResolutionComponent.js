3e182e9b6028272f701d9efa9b14d8fc48693845
gportas <guillermoportasgonzalez@gmail.com>
2018-03-08 20:27:30 +0100

    Exercise resolution component retrieving exercises by ajax call

--- web/src/components/student/ExerciseResolutionComponent.js
+++ web/src/components/student/ExerciseResolutionComponent.js
@@ -6,32 +6,67 @@ var React = require('react');
 var createReactClass = require('create-react-class');
 
 const ExerciseResolutionComponent = createReactClass({
+    componentWillMount() {
+        const self = this;
+        $.ajax({
+            url: "http://127.0.0.1:8000/exercises/",
+            dataType: 'json',
+            type: 'get',
+            crossDomain: true,
+            success: function (output, status, xhr) {
+                const data = xhr.responseText;
+                const jsonResponse = $.parseJSON(data);
+                self.setState({exercises: jsonResponse["data"]});
+            },
+            complete: function () {
+            }
+            //todo: Errors treatment!
+        });
+    },
+
+    componentDidMount() {
+        $("#submitButton").click(function(){
+            // todo: ajax Call to exercise correction
+        });
+    },
+
     changeHandler(solutionText) {
         $("#queryOutput").val(solutionText);
     },
 
     render() {
-        return (
-            <div>
-                <div className="common-div">
-                    <div className="exercise-resolution-left-div">
-                        <label className="common-label">Statement:</label>
-                        <hr className="exercise-resolution-divider"/>
-                        <label className="common-label">
-                            Here goes an exercise statement
-                        </label>
-                        <div className="exercise-resolution-left-bottom-div">
-                            <CommandLineComponent func={this.changeHandler} rows={6}/>
+        if (this.state && this.state.exercises) {
+            return (
+                <div>
+                    <div className="common-div">
+                        <div className="exercise-resolution-left-div">
+                            <label className="common-label">Statement:</label>
+                            <hr className="exercise-resolution-divider"/>
+                            <label className="common-label">
+                                {this.state.exercises[0]["question"]}
+                            </label>
+                            <div className="exercise-resolution-left-bottom-div">
+                                <CommandLineComponent func={this.changeHandler} rows={6}/>
+                            </div>
+                        </div>
+                        <div className="exercise-resolution-right-div">
+                            <label className="common-label">Solution:</label>
+                            <textarea readOnly={true} className="form-control query-text-area" id="queryOutput"
+                                      rows={18}/>
+                            <button type="submit" className="btn btn-success common-button" id="submitButton">Submit
+                                Solution
+                            </button>
                         </div>
                     </div>
-                    <div className="exercise-resolution-right-div">
-                        <label className="common-label">Solution:</label>
-                        <textarea readOnly={true} className="form-control query-text-area" id="queryOutput" rows={18}/>
-                        <button type="submit" className="btn btn-success common-button" id="submitButton">Submit Solution</button>
-                    </div>
                 </div>
-            </div>
-        );
+            );
+        } else {
+            // todo: Show loading
+            return (
+                <div>
+                </div>
+            )
+        }
     }
 });
 
